5c2d8f084e850945a8c450e4d9bb50d66fc578fd
Eric Ford <ebf11@psu.edu>
2020-08-23 05:32:20 -0400

    working on deps

--- src/instruments/neid/neid.jl
+++ src/instruments/neid/neid.jl
@@ -6,23 +6,9 @@ using DataFrames, FITSIO
 struct NEID1D <: AbstractInstrument1D end
 struct NEID2D <: AbstractInstrument2D end
 AnyNEID = Union{NEID1D,NEID2D}
-
-min_order(::NEID2D) = 1
-max_order(::NEID2D) = 90
-min_pixel_in_order(::NEID2D) = 1
-max_pixel_in_order(::NEID2D) = 9216
-
-min_pixel(::NEID1D) = 1
-max_pixel(::NEID1D) = 90*9216 # TODO: Update once know size of NEID's 1d extracted spectra
-
-orders_to_use_default(::NEID2D) = 1:60
-min_col_default(::NEID2D) = 451
-max_col_default(::NEID2D) = 9216 - (min_col_default(NEID2D())-1)
-
-metadata_symbols_default(::AnyNEID) = Symbol[:bjd, :target, :ssbz]
-metadata_strings_default(::AnyNEID) = String["OBSJD", "SKY-OBJ", "SSBZ000"]
-
 export NEID, NEID1D, NEID2D, AnyNEID
+
+include("traits.jl")
 export min_order, max_order, min_pixel_in_order, max_pixel_in_order
 export orders_to_use_default, min_col_default, max_col_default
 export metadata_symbols_default, metadata_strings_default

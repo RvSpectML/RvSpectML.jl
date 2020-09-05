77b91e5662e18ada5fbeb89370053f131aa760e8
Eric Ford <eford@psu.edu>
2020-09-04 20:23:09 -0400

    trying new ci workflow file

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,7 @@ makedocs(;
     authors="Eric Ford <ebf11@psu.edu> and contributors",
     repo="https://github.com/eford/RvSpectML.jl/blob/{commit}{path}#L{line}",
     sitename="RvSpectML.jl",
-    latest="main",
+#    latest="main",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://eford.github.io/RvSpectML.jl",

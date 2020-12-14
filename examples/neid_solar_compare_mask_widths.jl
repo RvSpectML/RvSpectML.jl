b446f7e13400fb7298a9fc9a09df2425fe7013ae
Eric Ford <ebf11@psu.edu>
2020-12-13 22:47:52 -0500

    update for new ccf_total opt args

--- examples/neid_solar_compare_mask_widths.jl
+++ examples/neid_solar_compare_mask_widths.jl
@@ -1,3 +1,4 @@
+if occursin(r"RvSpectMLEcoSystem$", pwd())   cd("RvSpectML")   end
 using Pkg
  Pkg.activate(".")
 
@@ -11,16 +12,17 @@ all_spectra = include(joinpath(pkgdir(EchelleInstruments),"examples/read_neid_so
 
 order_list_timeseries = extract_orders(all_spectra,pipeline_plan)
 
-line_list_df = prepare_line_list(linelist_for_ccf_filename, all_spectra, pipeline_plan,  v_center_to_avoid_tellurics=ccf_mid_velocity, Δv_to_avoid_tellurics = 21e3)
+linelist_for_ccf_fn_w_path = joinpath(pkgdir(EchelleCCFs),"data","masks","G2.espresso.mas")
+line_list_df = prepare_line_list(linelist_for_ccf_fn_w_path, all_spectra, pipeline_plan,  v_center_to_avoid_tellurics=ccf_mid_velocity, Δv_to_avoid_tellurics = 21e3)
 
-mask_scale_factors = [ 1.0,  2, 4, 6, 8, 10, 12, 14, 16 ]
+mask_scale_factors = [ 1.0,  2, 4, 6, 8, 10 ] # , 12, 14, 16 ]
  rms_rvs = zeros(length(mask_scale_factors))
  rms_binned_rvs = zeros(length(mask_scale_factors))
  println("Starting Tophat CCFs (w/o var)...")
  for (i,mask_scale_factor) in enumerate(mask_scale_factors)
    println("# mask_scale_factor = ", mask_scale_factor)
-   local (ccfs, v_grid) = ccf_total(order_list_timeseries, line_list_df, pipeline_plan, mask_scale_factor=mask_scale_factor, mask_type=:tophat,ccf_mid_velocity=ccf_mid_velocity, recalc=true, use_old = false)
-   alg_fit_rv = EchelleCCFs.MeasureRvFromCCFGaussian(frac_of_width_to_fit=3)
+   local (ccfs, v_grid) = ccf_total(order_list_timeseries, line_list_df, pipeline_plan, mask_scale_factor=mask_scale_factor, mask_type=:tophat,ccf_mid_velocity=ccf_mid_velocity, recalc=true)
+   alg_fit_rv = EchelleCCFs.MeasureRvFromCCFGaussian(frac_of_width_to_fit=2, init_guess_ccf_σ=ccf_mid_velocity)
    local rvs_ccf = calc_rvs_from_ccf_total(ccfs, pipeline_plan, v_grid=v_grid, times = order_list_timeseries.times, recalc=true, bin_consecutive=4, bin_nightly=false, alg_fit_rv=alg_fit_rv)
    rms_rvs[i] = std(rvs_ccf.-mean(rvs_ccf))
    rms_binned_rvs[i] = std(RvSpectML.bin_rvs_consecutive(rvs_ccf.-mean(rvs_ccf), 4))
@@ -55,13 +57,36 @@ mask_scale_factors2 = [ 1.0,  2, 4, 6, 8, 10, 12 ]
    ylabel!("RMS (m/s)")
  end
 
+#=
+((ccfs, ccf_vars), v_grid ) = ccf_total(order_list_timeseries, line_list_df, pipeline_plan, mask_type=:tophat, mask_scale_factor=4.0,  ccf_mid_velocity=ccf_mid_velocity, calc_ccf_var=true, recalc=true)
+(ccfs_norm, ccf_vars_norm) = EchelleCCFs.calc_normalized_ccfs(ccfs, ccf_vars)
+ccf_template = EchelleCCFs.calc_ccf_template(ccfs_norm, ccf_vars_norm)
+ccfs_sample_covar = 1.0*EchelleCCFs.calc_ccf_sample_covar(ccfs_norm, ccf_vars_norm, assume_normalized=true )
+mean_covar_on_diag = EchelleCCFs.RVFromCCF.compute_mean_off_diag(ccfs_sample_covar, 0)
+mrvt = EchelleCCFs.MeasureRvFromCCFTemplate(v_grid=v_grid, template=ccf_template, measure_width_at_frac_depth=0.5, frac_of_width_to_fit=3.0, mean_var=mean_covar_on_diag)
+(rvs_t, σ_rvs_t) = EchelleCCFs.measure_rvs_from_ccf(v_grid,ccfs_norm, ccf_vars_norm, alg=mrvt)
+rms_rv_binned = bin_rvs_consecutive(rvs_t.-mean(rvs_t),4)
+println("# RMS of RVs: ", std(rvs_t), "  binned RVs: ", std(rms_rv_binned), "  median(rvs_t): ", median(σ_rvs_t) )
+
+function triangular_shape_near_diagonal(n::Integer, width::Real; norm::Real = 1)
+  @. triangle_helper(x, p) = p[1]*max(1-abs(x)/p[2], 0)
+  shape_model = collect( triangle_helper(i-j,[norm,width]) for i in 1:n, j in 1:n )
+end
+shape_model = triangular_shape_near_diagonal(length(v_grid), 8)
+
+mrvt3 = EchelleCCFs.MeasureRvFromCCFTemplateNonDiagCovar(v_grid=v_grid, template=ccf_template, measure_width_at_frac_depth=0.5, frac_of_width_to_fit=2, mean_var=mean_covar_on_diag, near_diag_covar=shape_model )
+(rvs_t3, σ_rvs_t3) = EchelleCCFs.measure_rvs_from_ccf(v_grid,ccfs_norm, ccf_vars_norm, ccfs_sample_covar, alg=mrvt3)
+rms_rv_binned = bin_rvs_consecutive(rvs_t3.-mean(rvs_t3),4)
+println("# RMS of RVs: ", std(rvs_t3), "  binned RVs: ", std(rms_rv_binned), "  median(σ_rvs_t): ", median(σ_rvs_t3) )
+=#
+
 mask_scale_factors3 = [ 1.0,  2, 4, 6, 8, 10, 12 ]
  rms_rvs3 = zeros(length(mask_scale_factors3))
  rms_binned_rvs3 = zeros(length(mask_scale_factors3))
   println("Starting Half Cos...")
   for (i,mask_scale_factor) in enumerate(mask_scale_factors3)
     println("# mask_scale_factor = ", mask_scale_factor)
-    (ccfs_expr, v_grid_expr) = ccf_total(order_list_timeseries, line_list_df, pipeline_plan, mask_type=:halfcos, mask_scale_factor=mask_scale_factor,  ccf_mid_velocity=ccf_mid_velocity, recalc=true, use_old = false)
+    (ccfs_expr, v_grid_expr) = ccf_total(order_list_timeseries, line_list_df, pipeline_plan, mask_type=:halfcos, mask_scale_factor=mask_scale_factor,  ccf_mid_velocity=ccf_mid_velocity, recalc=true)
     rvs_ccf_expr = calc_rvs_from_ccf_total(ccfs_expr, pipeline_plan, v_grid=v_grid_expr, times = order_list_timeseries.times, recalc=true, bin_consecutive=4, bin_nightly=false)
     rms_rvs3[i] = std(rvs_ccf_expr.-mean(rvs_ccf_expr))
     rms_binned_rvs3[i] = std(bin_rvs_consecutive(rvs_ccf_expr.-mean(rvs_ccf_expr), 4))
@@ -80,7 +105,7 @@ mask_scale_factors4 = [ 1.0,  2.0, 3.0, 4.0, 4.5, 5.0, 5.5, 6.0 ]
    println("Starting Gaussian...")
     for (i,mask_scale_factor) in enumerate(mask_scale_factors4)
       println("# mask_scale_factor = ", mask_scale_factor)
-      (ccfs_expr, v_grid_expr) = ccf_total(order_list_timeseries, line_list_df, pipeline_plan, mask_type=:gaussian, mask_scale_factor=mask_scale_factor,  ccf_mid_velocity=ccf_mid_velocity, recalc=true, use_old = false)
+      (ccfs_expr, v_grid_expr) = ccf_total(order_list_timeseries, line_list_df, pipeline_plan, mask_type=:gaussian, mask_scale_factor=mask_scale_factor,  ccf_mid_velocity=ccf_mid_velocity, recalc=true)
       rvs_ccf_expr = calc_rvs_from_ccf_total(ccfs_expr, pipeline_plan, v_grid=v_grid_expr, times = order_list_timeseries.times, recalc=true, bin_consecutive=4, bin_nightly=false)
       rms_rvs4[i] = std(rvs_ccf_expr.-mean(rvs_ccf_expr))
       rms_binned_rvs4[i] = std(bin_rvs_consecutive(rvs_ccf_expr.-mean(rvs_ccf_expr), 4))

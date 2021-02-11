ef0a5b59650a9d120bd1ab0b0cd42bb2011d4c95
alexander-wise <esiwrednaxela@gmail.com>
2021-02-10 23:04:44 -0500

    bug fixes for simulated EXPRES data pipeline

--- src/pipeline/extract_orders.jl
+++ src/pipeline/extract_orders.jl
@@ -4,7 +4,7 @@ function extract_orders(data::AbstractVector{ST}, pipeline::PipelinePlan; recalc
       if verbose println("# Extracting order list timeseries from spectra.")  end
       @assert !need_to(pipeline,:read_spectra)
       order_list_timeseries = make_order_list_timeseries(data, orders_to_use=orders_to_use)
-      order_list_timeseries = filter_bad_chunks(order_list_timeseries,verbose=true)
+      order_list_timeseries = filter_bad_chunks(order_list_timeseries,verbose=verbose)
       #RvSpectML.normalize_spectra!(order_list_timeseries,data);
       set_cache!(pipeline,:extract_orders,order_list_timeseries)
       dont_need_to!(pipeline,:extract_orders)

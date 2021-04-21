f1d396474d4535a9d09f684c662993ded9b164c4
Eric Ford <ebf11@psu.edu>
2021-04-21 13:27:39 -0400

    generalize calc_solar_alt

--- src/util/sun.jl
+++ src/util/sun.jl
@@ -1,7 +1,15 @@
-function calc_solar_alt(time::Real)
-  obs = AstroLib.observatories["kpno"]
-   (ra_sun, dec_sun, ) = sunpos(time)
-   lst = ct2lst(obs.longitude,time)
-   (alt, az) = hadec2altaz(lst-ra_sun/360*24, dec_sun,obs.latitude)
-   return alt
+function calc_solar_alt(time::Real; loc::Symbol)
+  #=
+	obs = AstroLib.observatories["kpno"]
+	lat = obs.latitude
+	long = obs.longitude
+	=#
+  # Values for specifically for WIYN
+  long = -111.600562
+  lat =  31.958092
+  alt =  2091.0
+  (ra_sun, dec_sun, ) = sunpos(time)
+  lst = ct2lst(long,time)
+  (alt, az) = hadec2altaz(lst-ra_sun/360*24, dec_sun,lat)
+  return alt
 end
